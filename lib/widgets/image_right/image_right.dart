109282bb222eae6b5f64abd7581dc3a563fc8f4e
Dylan Roubos <dylanroubos@Dylans-MacBook-Pro.local>
2021-04-11 11:44:49 +0200

    Added mobile support for the image

--- lib/widgets/image_right/image_right.dart
+++ lib/widgets/image_right/image_right.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ImageRight extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      child: SizedBox(
-        child: Image.network('assets/assets/meme.jpg'),
-      ),
-    );
+    if (kIsWeb) {
+      return Container(
+        width: 600,
+        child: SizedBox(
+          child: Image.network('assets/assets/meme.jpg'),
+        ),
+      );
+    } else {
+      return Container(
+        width: 600,
+        child: SizedBox(
+          child: Image.asset('assets/meme.jpg'),
+        ),
+      );
+    }
   }
 }
